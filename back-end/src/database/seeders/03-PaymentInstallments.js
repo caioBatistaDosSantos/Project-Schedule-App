7b089d50792b9092f7bbb69235a2ca2792df53c6
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-09-02 01:35:05 -0300

    feat: migration, model and seeder the payment installments

---
+++ back-end/src/database/seeders/03-PaymentInstallments.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkInsert('payment_installments',
+      [
+        {
+          id: 1,
+          queryId: 3,
+          installmentsPrice: 1000,
+          date: new Date('2022-10-02T19:58:00.000Z'),
+          status: false,
+        },
+        {
+          id: 2,
+          queryId: 3,
+          installmentsPrice: 1000,
+          date: new Date('2022-11-02T19:58:00.000Z'),
+          status: false,
+        },
+        {
+          id: 3,
+          queryId: 3,
+          installmentsPrice: 1000,
+          date: new Date('2022-12-02T19:58:00.000Z'),
+          status: false,
+        },
+      ], { timestamps: false });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkDelete('payment_installments', null, {});
+  },
+};
