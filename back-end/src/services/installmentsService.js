c8af91f0e0c47fe20a15ffd0de8d7ba240a62a8d
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-09-06 21:31:53 -0300

    feat: add InstallmentsService

---
+++ back-end/src/services/installmentsService.js
@@ -0,0 +1,34 @@
+const { Op } = require('sequelize');
+const { PaymentInstallment } = require('../database/models');
+
+const getPaymentInstallments = async (id) => {
+  const paymentInstallments = await PaymentInstallment.findAll({ where: { queryId: id } });
+
+  return paymentInstallments;
+};
+
+const getPaymentInstallmentsByDate = async (id, date) => {
+  const paymentInstallments = await PaymentInstallment
+    .findAll({
+      where: {
+        [Op.and]: [
+          { userId: id },
+          { date: { [Op.lte]: date } },
+        ],
+      },
+    });
+
+  return paymentInstallments;
+};
+
+const updatePaymentInstallments = async (id, status) => {
+  const updatedStatus = await PaymentInstallment.update({ status }, { where: { id } });
+
+  return updatedStatus;
+};
+
+module.exports = {
+  getPaymentInstallments,
+  getPaymentInstallmentsByDate,
+  updatePaymentInstallments,
+};
