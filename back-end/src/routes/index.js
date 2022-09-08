5e7d9dc3a4c2d5b9164570b2960864b8141c3205
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-09-08 11:36:44 -0300

    feat: add routes installments

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -5,10 +5,19 @@ const validateName = require('../middlewares/validateName');
 const validateJWT = require('../middlewares/jwt');
 
 const userController = require('../controllers/userController');
+const installmentsController = require('../controllers/installmentsController');
 
 const router = express.Router();
 
 router.post('/login', validateEmailAndPass, userController.loginUser);
 router.post('/register', validateEmailAndPass, validateName, userController.createUser);
+
+router.get('/installments/:id', validateJWT, installmentsController.getPaymentInstallments);
+router.get(
+  '/search/installments/:date',
+  validateJWT,
+  installmentsController.getPaymentInstallmentsByDate,
+);
+router.put('/installments/:id', validateJWT, installmentsController.updatePaymentInstallments);
 
 module.exports = router;
