eaee93aae8edf0976cdae4fd4f29a487f8e6fa85
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-09-08 19:05:41 -0300

    test: add test routes POST and GET /queries

--- back-end/src/tests/utilForTests.js
+++ back-end/src/tests/utilForTests.js
@@ -31,10 +31,49 @@ const REGISTER_MATCHER = {
 	"email": "brett@email.com",
 }
 
+const QUERIES_MATCHER = [
+	{
+		"id": 8,
+		"userId": 1,
+		"patientsName": "teste fulano",
+		"descripition": "fazendo teste",
+		"totalPrice": "3000.00",
+		"date": "2022-09-08T17:51:28.000Z",
+		"optionPayment": "Crédito",
+		"methodPayment": "À vista"
+	},
+	{
+		"id": 9,
+		"userId": 1,
+		"patientsName": "teste fulano",
+		"descripition": "fazendo teste",
+		"totalPrice": "3000.00",
+		"date": "2022-09-08T17:51:29.000Z",
+		"optionPayment": "Crédito",
+		"methodPayment": "À vista"
+	}
+];
+
+const	NEW_QUERIE_MATCHER = {
+	"id": 13,
+	"patientsName": "test patientsName",
+	"descripition": "test descripition",
+	"totalPrice": 3000,
+	"optionPayment": "Crédito",
+	"methodPayment": "À vista",
+	"date": "2022-09-08T18:17:56.611Z",
+	"userId": 1
+}
+
+const DECODE_MATCHER = { data: { id: 1 } };
+
 module.exports = {
   LOGIN_MATCHER,
   TOKEN_MATCHER,
 	USER_MATCHER,
 	CREATE_USER_MATCHER,
 	REGISTER_MATCHER,
+	QUERIES_MATCHER,
+	DECODE_MATCHER,
+	NEW_QUERIE_MATCHER,
 }
