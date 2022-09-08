182ac4b52f1c3d4419dcd853d010e1ceb192ad83
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-09-08 12:08:15 -0300

    feat: add create Queries in querieService

--- back-end/src/services/queriesService.js
+++ back-end/src/services/queriesService.js
@@ -1,59 +1,10 @@
 const Sequelize = require('sequelize');
 const { StatusCodes } = require('http-status-codes');
 const config = require('../database/config/config');
-const { Query } = require('../database/models');
+const { Query, PaymentInstallment } = require('../database/models');
 const objectError = require('../utils/objectError');
 
 const sequelize = new Sequelize(config.development);
-
-// const createSale = async (params) => {
-//   try {
-//     const { productIds, ...saleParams } = params;
-//     return await sequelize.transaction(async (t) => {
-//       const sale = await Sale.create({
-//         ...saleParams, status: 'Pendente', saleDate: new Date(),
-//       }, { transaction: t });
-
-//       await SalesProduct.bulkCreate(
-//         productIds.map(({ itemId: productId, itemQtd: quantity }) => ({
-//           saleId: sale.id, productId, quantity,
-//         })), { transaction: t },
-//       );
-
-//       return sale; 
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     throw objectError(StatusCodes.BAD_REQUEST, 'Error making the sale. Try again later');
-//   }
-// };
-
-// const getSaleByRole = async (userId, roleUser) => {
-//   const role = roleUser === 'customer' ? 'userId' : 'sellerId';
-
-//   const sales = await Sale.findAll({ where: { [role]: userId } });
-
-//   return sales;
-// };
-
-// const getSaleById = async (id) => {
-//   const sale = await Sale.findByPk(id,
-//    { include: [
-//       { model: Product, as: 'products', through: {} },
-//     ],
-//   });
-
-//   if (!sale) throw objectError(StatusCodes.NOT_FOUND, 'Sale does not exist');
-
-//   return sale;
-// };
-
-// const updateSaleById = async (id, status) => {
-//   await Sale.update(
-//     { status },
-//     { where: { id } },
-//   );
-// };
 
 const getQueriesByUser = async (id) => {
   const queries = await Query.findAll({ where: { userId: id } });
@@ -65,7 +16,31 @@ const deleteQueriesByUser = async (id) => {
   await Query.destroy({ where: { userId: id } });
 };
 
+const createQueries = async (params) => {
+  try {
+    const { dates, installmentsPrice, id, ...Params } = params;
+
+    return await sequelize.transaction(async (t) => {
+      const queries = await Query.create({
+        ...Params, date: new Date(), userId: id,
+      }, { transaction: t });
+
+      await PaymentInstallment.bulkCreate(
+        dates.map((e) => ({
+          queryId: queries.id, userId: id, installmentsPrice, date: e, status: false,
+        })), { transaction: t },
+      );
+
+      return queries; 
+    });
+  } catch (error) {
+    console.log(error);
+    throw objectError(StatusCodes.BAD_REQUEST, 'Error making the queries. Try again later');
+  }
+};
+
 module.exports = {
   getQueriesByUser,
   deleteQueriesByUser,
+  createQueries,
 };
