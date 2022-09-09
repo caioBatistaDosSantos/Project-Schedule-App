52c87adda7a06774e7818f9d61594a266af5ebfa
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-09-09 20:37:59 -0300

    feat: add route Home

--- front-end/src/routes/app.routes.jsx
+++ front-end/src/routes/app.routes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Login from '../screens/Login';
 import Redirect from '../screens/Redirect';
 import Register from '../screens/Register';
+import Home from '../screens/Home';
 
 export default function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ export default function AppRoutes() {
       <Route path="/" element={ <Redirect /> } />
       <Route path="/login" element={ <Login /> } />
       <Route path="/register" element={ <Register /> } />
+      <Route path="/home" element={ <Home /> } />
     </Routes>
   );
 }
---
+++ front-end/src/screens/Home/index.jsx
@@ -0,0 +1,10 @@
+import NavBar from '../../components/NavBar';
+
+export default function Home() {
+  return (
+    <>
+      <NavBar Route="home" />
+      <h3>Home</h3>
+    </>
+  );
+}
