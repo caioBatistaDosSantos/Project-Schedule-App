9887108aa6f4fdd469379f41fa8f7b0819f08a96
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-09-09 20:37:22 -0300

    feat: add component NavBar

---
+++ front-end/src/components/NavBar.jsx
@@ -0,0 +1,69 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { GET } from '../utils/requestApi';
+import { MESSAGE_TOKEN_EXPIRED, BTNS_NAV_BAR } from '../utils/conts';
+import { getLocalStorage, logout, redirectRoute } from '../utils/functions';
+
+export default function NavBar({ Route }) {
+  const [user, setUser] = useState({});
+
+  const getUser = async () => {
+    try {
+      const USER = getLocalStorage('user');
+
+      if (!USER) throw new Error('user not found');
+
+      await GET('/logged', {
+        headers: { Authorization: USER.token },
+      });
+
+      setUser(USER);
+    } catch (error) {
+      console.log(error.message);
+      redirectRoute('/');
+      alert(MESSAGE_TOKEN_EXPIRED);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const createBtn = ({ nameBtn, routeBtn }) => (
+    <button
+      key={ routeBtn }
+      type="button"
+      onClick={ () => redirectRoute(`/${routeBtn}`) }
+      disabled={ Route === routeBtn }
+      data-testid={ `element-navbar-btn-${routeBtn}` }
+    >
+      {`${nameBtn}`}
+    </button>
+  );
+
+  return (
+    <nav>
+      <h4 data-testid="element-navbar-user-name">
+        {user.name}
+      </h4>
+      <button
+        type="button"
+        onClick={ () => {
+          logout();
+          redirectRoute('/login');
+        } }
+        data-testid="element-navbar-btn-logout"
+      >
+        Sair
+      </button>
+      <br />
+      <section>
+        { BTNS_NAV_BAR.map((e) => createBtn(e)) }
+      </section>
+    </nav>
+  );
+}
+
+NavBar.propTypes = {
+  Route: PropTypes.string.isRequired,
+};
--- front-end/src/utils/conts.js
+++ front-end/src/utils/conts.js
@@ -1,3 +1,10 @@
 export const TWELVE = 12;
 export const SIX = 6;
 export const EMAIL_REGEX = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+export const MESSAGE_TOKEN_EXPIRED = `Token expirado ou não encontrado!
+Por favor realize o login novamente`;
+export const BTNS_NAV_BAR = [
+  { nameBtn: 'Home', routeBtn: 'home' },
+  { nameBtn: 'Cadastrar nova consulta', routeBtn: 'register-query' },
+  { nameBtn: 'Gerenciamento financeiro', routeBtn: 'financial-management' },
+];
