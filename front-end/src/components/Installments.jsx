a8465e685a14ca405efec419e4d4a2e8d82b0ecb
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-09-10 01:34:17 -0300

    feat: create component Installments

---
+++ front-end/src/components/Installments.jsx
@@ -0,0 +1,54 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { PUT } from '../utils/requestApi';
+
+export default function Installments({
+  id,
+  installmentsPrice,
+  date,
+  status,
+  index,
+}) {
+  const [checkedStatus, setCheckedStatus] = useState(status);
+
+  const INDEX = index + 1;
+  const PRICE = installmentsPrice.replace(/\./, ',');
+  const DATE = new Date(date).toLocaleDateString('pt-BR');
+  const STATUS = checkedStatus ? 'pago' : 'pendente';
+
+  const checkInstallments = async () => {
+    try {
+      const TOKEN = localStorage.getItem('token');
+
+      await PUT(
+        `/installments/${id}`,
+        { status: !checkedStatus },
+        { headers: { authorization: TOKEN } },
+      );
+
+      setCheckedStatus(!checkedStatus);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <label htmlFor={ index }>
+      {`${INDEX}x - R$${PRICE} - ${DATE} - ${STATUS}`}
+      <input
+        type="checkbox"
+        id={ index }
+        defaultChecked={ status }
+        onClick={ () => checkInstallments() }
+      />
+    </label>
+  );
+}
+
+Installments.propTypes = {
+  id: PropTypes.number.isRequired,
+  installmentsPrice: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  status: PropTypes.bool.isRequired,
+  index: PropTypes.number.isRequired,
+};
