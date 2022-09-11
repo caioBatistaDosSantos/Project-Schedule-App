9e9b0ccffc4e3bda465205b6443473895c5c0706
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-09-11 18:35:21 -0300

    fix: remove btn Sair in NavBar

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,35 +1,9 @@
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
-import { GET } from '../utils/requestApi';
-import { MESSAGE_TOKEN_EXPIRED, BTNS_NAV_BAR } from '../utils/conts';
-import { getLocalStorage, logout, redirectRoute } from '../utils/functions';
+import ItensForNavBar from './ItensForNavBar';
+import { BTNS_NAV_BAR } from '../utils/conts';
+import { redirectRoute } from '../utils/functions';
 
 export default function NavBar({ Route }) {
-  const [user, setUser] = useState({});
-
-  const getUser = async () => {
-    try {
-      const USER = getLocalStorage('user');
-
-      if (!USER) throw new Error('user not found');
-
-      await GET('/logged', {
-        headers: { Authorization: USER.token },
-      });
-
-      setUser(USER);
-    } catch (error) {
-      console.log(error.message);
-      redirectRoute('/');
-      logout();
-      alert(MESSAGE_TOKEN_EXPIRED);
-    }
-  };
-
-  useEffect(() => {
-    getUser();
-  }, []);
-
   const createBtn = ({ nameBtn, routeBtn }) => (
     <button
       key={ routeBtn }
@@ -39,25 +13,15 @@ export default function NavBar({ Route }) {
       data-testid={ `element-navbar-btn-${routeBtn}` }
     >
       {`${nameBtn}`}
+      <ItensForNavBar
+        RouteComponent={ routeBtn }
+        RouteScreen={ Route }
+      />
     </button>
   );
 
   return (
     <nav>
-      <h4 data-testid="element-navbar-user-name">
-        {user.name}
-      </h4>
-      <button
-        type="button"
-        onClick={ () => {
-          logout();
-          redirectRoute('/login');
-        } }
-        data-testid="element-navbar-btn-logout"
-      >
-        Sair
-      </button>
-      <br />
       <section>
         { BTNS_NAV_BAR.map((e) => createBtn(e)) }
       </section>
