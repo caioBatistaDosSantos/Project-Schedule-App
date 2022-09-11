39c79bd83a307ba7c40770545e34e3080c82b7e4
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-09-10 23:01:56 -0300

    feat: create screen FinancialManagement

---
+++ front-end/src/screens/FinancialManagement/index.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import { GET } from '../../utils/requestApi';
+import { createArrayOfNumbers } from '../../utils/functions';
+import { TWELVE } from '../../utils/conts';
+import NavBar from '../../components/NavBar';
+import FinancialDatas from '../../components/FinancialDatas';
+
+export default function FinancialManagement() {
+  const [data, setData] = useState(false);
+  const [numbersMonts, setNumberMonths] = useState(1);
+
+  const getDate = () => {
+    const today = new Date();
+    const searchedDate = new Date();
+
+    searchedDate
+      .setMonth(today.getMonth() + Number(numbersMonts));
+
+    return searchedDate.toLocaleString('pt-BR').replace(/[/]/gi, '-');
+  };
+
+  const searchData = async () => {
+    try {
+      const searchedDate = getDate();
+      const TOKEN = localStorage.getItem('token');
+
+      const result = await GET(
+        `/search/installments/${searchedDate}`,
+        { headers: { authorization: TOKEN } },
+      );
+
+      setData(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const numberMonth = () => {
+    const NUMBERS_MONTH = createArrayOfNumbers(1, TWELVE);
+
+    return NUMBERS_MONTH.map((e) => (
+      <option
+        key={ e }
+        id={ e }
+      >
+        {e}
+      </option>
+    ));
+  };
+
+  return (
+    <>
+      <NavBar Route="financial-management" />
+      <h3>Financial Management</h3>
+      <main>
+        <label htmlFor="select-number-month">
+          Informe a quantidade de mêses que deseja realizar a pesquisa:
+          <select
+            data-testid="select-number-month"
+            name="select-number-month"
+            id="select-number-month"
+            required
+            value={ numbersMonts }
+            onChange={ ({ target }) => setNumberMonths(target.value) }
+          >
+            { numberMonth() }
+          </select>
+        </label>
+        <button
+          type="button"
+          data-testid="button-search"
+          onClick={ () => searchData() }
+        >
+          Pesquisar
+        </button>
+        {
+          data && (
+            <div>
+              <span>{`Resultado da pesquisa até ${getDate()}`}</span>
+              <FinancialDatas data={ data } />
+            </div>
+          )
+        }
+      </main>
+    </>
+  );
+}
