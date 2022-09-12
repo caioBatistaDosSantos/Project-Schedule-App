22a11c2fd2451f1d9928d9d503010678e354b2ab
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-09-12 16:32:47 -0300

    feat: add styles in screen Home

--- front-end/src/screens/Home/index.jsx
+++ front-end/src/screens/Home/index.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
+import { MdOutlineDeleteForever } from 'react-icons/md';
 import { GET, DELETE } from '../../utils/requestApi';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import CardQueries from '../../components/CardQueries';
+import './styles.css';
 
 export default function Home() {
   const [queries, setQueries] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getQueries = async () => {
     try {
@@ -14,8 +17,10 @@ export default function Home() {
       const QUERIES = await GET('/queries', { headers: { authorization: TOKEN } });
 
       setQueries(QUERIES);
+      setLoading(false);
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   };
 
@@ -36,23 +41,29 @@ export default function Home() {
     }
   };
 
+  if (loading) return <span><i>Carregando...</i></span>;
+
   return (
     <>
       <Header Route="Home" />
-      <main>
-        <span><i>Consultas cadastradas</i></span>
+      <main className="container-home">
+        <span><i>Consultas cadastradas...</i></span>
         {
           queries.length === 0
             ? <h4><i>Ainda não há nenhuma consulta cadastrada</i></h4>
             : queries.map((e) => (
-              <div key={ e.id }>
+              <div
+                key={ e.id }
+                className="container-query"
+              >
                 <CardQueries { ...e } />
                 <button
                   type="button"
                   data-testid="button-remove-querie"
                   onClick={ () => btnRemove(e.id) }
+                  className="btn-query"
                 >
-                  Excluir
+                  <MdOutlineDeleteForever className="btn-query-icon" />
                 </button>
               </div>
             ))
